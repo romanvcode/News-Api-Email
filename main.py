8cca0de9a02a7d8b0f0cf044af2027211d3e0ee1
r1dercode <roman.v.ua@gmail.com>
2023-08-26 14:57:44 +0300

    Send an email with news titles and description

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = "2b5b2732f80e4fb9bc2bdbf3e257a2eb"
 url = ("https://newsapi.org/v2/everything?q=tesla&"
@@ -12,6 +13,9 @@ request = requests.get(url)
 content = request.json()
 
 # Access the article titles and description
+text = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    text = text + article["title"] + "\n" + article["description"] + 2 * "\n"
+
+text = text.encode("utf-8")
+send_email(text)
